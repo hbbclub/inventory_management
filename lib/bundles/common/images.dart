f97896a2b0f670e0cee98bc5f2cd22f8ca3f39c1
jinWarr <baij930312@163.com>
2019-02-26 15:59:20 +0800

    add print image

--- lib/bundles/common/images.dart
+++ lib/bundles/common/images.dart
@@ -1,5 +1,8 @@
 class ImageAssets {
   static final String materalIcon = 'assets/images/materal_icon.png';
+  static final String print1 = 'assets/images/print_log_1.png';
+  static final String print2 = 'assets/images/print_log_2.png';
+  static final String print3 = 'assets/images/print_log_3.png';
 }
 
 final ImageAssets imagePaths = ImageAssets();

630728a2fb39eab4acbee60b79b153f621804594
jinWarr <2368276566@qq.com>
2019-03-01 23:50:14 +0800

    add refresh progress indicator

--- lib/bundles/material/search_material.dart
+++ lib/bundles/material/search_material.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:inventory_management/bundles/bloc/bloc_provider.dart';
 import 'package:inventory_management/bundles/material/material_model.dart';
 
-
 class SearchMaterial<T> extends SearchDelegate<String> {
   AppBloc bloc;
   SearchMaterial(this.bloc);
@@ -25,7 +24,31 @@ class SearchMaterial<T> extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return null;
+    bloc.handelSearchTextChanged(query);
+    return StreamBuilder(
+      stream: bloc.suggestChangedStream,
+      builder:
+          (BuildContext context, AsyncSnapshot<List<MaterialModel>> snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                onTap: () {
+                  bloc.resultModel = snapshot.data[index];
+                  close(context, null);
+                },
+                title: Text(snapshot.data[index].partNo),
+              );
+            },
+          );
+        } else {
+          return Center(
+            child: RefreshProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 
   @override

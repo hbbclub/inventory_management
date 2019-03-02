1dc4f5a5f2bcb6b18c31652f2be5ebe7b25b5882
jinWarr <baij930312@163.com>
2019-03-02 13:11:48 +0800

    add upload image api

--- lib/bundles/agent/api.dart
+++ lib/bundles/agent/api.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:inventory_management/bundles/agent/agent.dart';
 import 'package:inventory_management/bundles/agent/api_model.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 export 'package:inventory_management/bundles/agent/api_model.dart';
 
 class Api {
@@ -8,18 +11,19 @@ class Api {
     keyword,
   }) async {
     return httpUtil.get(
-      '/material/list',
+      '/mm/material/list',
       params: {
         'keyword': keyword ?? '',
       },
     );
   }
+
   //物料详情
   Future<ApiModel> materialDetail({
     partNo,
   }) async {
     return httpUtil.get(
-      '/material/details',
+      '/mm/material/details',
       params: {
         'part_no': partNo ?? '',
       },
@@ -29,8 +33,23 @@ class Api {
 //标签列表
   Future<ApiModel> labelList() async {
     return httpUtil.get(
-      '/label/list',
+      '/mm/label/list',
       params: {},
+    );
+  }
+
+  //上传文件
+  Future<ApiModel> fileUpload(List<Asset> files) async {
+    List<UploadFileInfo> infos = List.generate(files.length, (index) {
+      return UploadFileInfo.fromBytes(
+          files[index].thumbData.buffer.asUint8List(), 'file');
+    });
+    FormData formData = FormData.from({
+      "file": infos,
+    });
+    return httpUtil.post(
+      '/mm/file/upload',
+      params: formData,
     );
   }
 }

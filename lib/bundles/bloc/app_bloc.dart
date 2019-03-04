5a8d01f1da364ba6c3bf11b13451661545d96d2d
jinWarr <baij930312@163.com>
2019-03-04 14:26:12 +0800

    to achieve singleton pattern in app bloc

--- lib/bundles/bloc/app_bloc.dart
+++ lib/bundles/bloc/app_bloc.dart
@@ -1,11 +1,17 @@
 import 'package:inventory_management/bundles/bloc/bloc_provider.dart';
 
 class AppBloc extends BlocBase {
- 
+  //私有构造方法
+  AppBloc._internal();
+
+  //保存单例
+  static AppBloc _singleton = AppBloc._internal();
+
+  //工厂方法
+  factory AppBloc() => _singleton;
+
   @override
-  void dispose() {
-   
-  }
+  void dispose() {}
 }
 
 final appBloc = AppBloc();

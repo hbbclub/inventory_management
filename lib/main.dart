60276611527a16e486d9a7384f0f0d7e5f9f1312
jinWarr <2368276566@qq.com>
2019-03-09 22:50:49 +0800

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:inventory_management/bundles/bloc/bloc_provider.dart';
-import 'package:inventory_management/bundles/home/home.dart';
+import 'package:inventory_management/bundles/login/login_page.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Inventory Management',
-      home: BlocProvider(
-        bloc: appBloc,
-        child: Home(),
+      theme: ThemeData(
+        textTheme: TextTheme(
+          body1: TextStyle(
+            fontSize: 16,
+            color: Colors.black87,
+          ),
+          body2: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        
       ),
+      home: LoginPage(),
     ));

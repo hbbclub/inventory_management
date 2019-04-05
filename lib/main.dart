6c48a66cdd5f688be94374b26f3b83f44a5606cb
jinWarr <baij930312@163.com>
2019-04-05 14:35:54 +0800

    add fish redux package

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:inventory_management/bundles/common/colors.dart';
-import 'package:inventory_management/bundles/home/home.dart';
+import 'package:fish_redux/fish_redux.dart';
 import 'package:inventory_management/bundles/home/welcome.dart';
 
 void main() async {
-  runApp(MaterialApp(
-    // debugShowCheckedModeBanner: false,
-    title: 'Inventory Management',
-    theme: ThemeData(
-      
-      primaryColor: mainColor,
-    
-      textTheme: TextTheme(
-        body1: TextStyle(
-          fontSize: 16,
-          color: Colors.black87,
-        ),
-        body2: TextStyle(
-          fontSize: 20,
-          color: Colors.black,
+  runApp(AppProvider(
+    child: MaterialApp(
+      // debugShowCheckedModeBanner: false,
+      title: 'Inventory Management',
+      theme: ThemeData(
+        primaryColor: mainColor,
+        textTheme: TextTheme(
+          body1: TextStyle(
+            fontSize: 16,
+            color: Colors.black87,
+          ),
+          body2: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
         ),
       ),
+      home: Welcome(),
     ),
-    home: Welcome(),
   ));
 }

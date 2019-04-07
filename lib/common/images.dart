06f423c7360ab46afe7e443c5efb375254bb933a
jinWarr <baij930312@163.com>
2019-04-07 16:19:43 +0800

    add setting image

--- lib/common/images.dart
+++ lib/common/images.dart
@@ -7,4 +7,8 @@ class ImageAssets {
   static final String main2 = 'assets/images/main_2.png';
   static final String main3 = 'assets/images/main_3.png';
   static final String main4 = 'assets/images/main_4.png';
+  static final String setting1 = 'assets/images/setting_1.png';
+  static final String setting2 = 'assets/images/setting_2.png';
+  static final String setting3 = 'assets/images/setting_3.png';
+  static final String setting4 = 'assets/images/setting_4.png';
 }

f98bc2b47b98119e953d6c16c0c8302b5258b4b1
jinWarr <baij930312@163.com>
2019-04-20 08:06:58 +0800

    update fish redux version

--- plugin/printer/test/printer_test.dart
+++ plugin/printer/test/printer_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:printer/printer.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('printer');
